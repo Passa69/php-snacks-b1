25da2fe4fc5b7143987f3d6da8042637ca343d03
tommaso <passarotto13@gmail.com>
2021-12-20 17:01:19 +0100

    creazione array

---
+++ index3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>phpSnack3</title>
+</head>
+<body>
+    <?php
+        $posts = [
+            '10/01/2021' => [
+                [
+                    'title' => 'Post 1',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 1'
+                ],
+                [
+                    'title' => 'Post 2',
+                    'author' => 'Roberto Bello',
+                    'text' => 'Testo post 2'
+                ],
+            ],
+            '13/04/2020' => [
+                [
+                    'title' => 'Post 3',
+                    'author' => 'Giulia Reggia',
+                    'text' => 'Testo post 3'
+                ]
+            ]
+        ];
+    ?>
+</body>
+</html>
