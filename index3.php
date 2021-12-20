80ab8e21aad396f241d34b10378f9ff676ddf4ff
tommaso <passarotto13@gmail.com>
2021-12-20 17:50:38 +0100

    tentativo di ciclo per stampa dei posts

--- index3.php
+++ index3.php
@@ -27,12 +27,37 @@
                     'author' => 'Giulia Reggia',
                     'text' => 'Testo post 3'
                 ]
-            ]
+            ],
+            '15/05/2019' => [
+                [
+                    'title' => 'Post 4',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 4'
+                ],
+                [
+                    'title' => 'Post 5',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 5'
+                ],
+                [
+                    'title' => 'Post 6',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 6'
+                ]
+            ],
         ];
 
-        for ($x=0; $x<count($posts); $x++) {
-            // echo array_keys($posts);
-            echo $posts['10/01/2021'][$x]['title']. '<br>' . $posts['10/01/2021'][$x]['author']. '<br>' . $posts['10/01/2021'][$x]['text']. '<br>';
+        $keys = array_keys($posts);
+        for ($x=0; $x<count($keys); $x++) {
+            $key = $keys[$x];
+            $postsByKey = $posts[$key];
+            for ($i=0; $i<$postsByKey; $i++) {
+                $value1 = $posts[$key][$i]['title'];
+                // $value2 = $posts[$key][$i]['author'];
+                // $value3 = $posts[$key][$i]['text'];
+            };
+
+            echo $key . '<br>' . $value1 . '<br>';
         }
     ?>
 </body>
